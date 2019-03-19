873f5f53af181965b02922499fa7d76e0fb9fba7
Jhovany Hernandez <jhovanyhernandezr@gmail.com>
2019-03-19 16:06:53 +0000

    browser-sync-webpack-plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,5 +12,12 @@ module.exports = {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
-  }
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        server: { baseDir: ['.'] }
+    })
+  ]
 };
